92a37bfde9c8ce04968a833585ef4ae1205dca8d
Shidhar25 <150617921+Shidhar25@users.noreply.github.com>
2023-12-01 13:36:07 +0530

    Add files via upload

---
+++ javascript_1.js
@@ -0,0 +1,14 @@
+
+
+function test(){
+    var a= "3";
+    var b= "8"
+    var c= a;
+    a=b;
+    b=c;
+
+    console.log("a is "+a);
+    console.log("b is "+ b);
+
+}
+document.querySelector("#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.w-full.pt-2.md\\:pt-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:w-\\[calc\\(100\\%-\\.5rem\\)\\] > form > div > div > div > button")
